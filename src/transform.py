df9b21ce7fcb5eeacfb28ef1173ad46c3f214415
Jeroen van Veen <j.van.veen@rug.nl>
2019-12-09 15:14:57 +0100

    fix: upgrade to the latest csv format

--- src/transform.py
+++ src/transform.py
@@ -15,35 +15,36 @@ log = logging.getLogger(__name__)
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
-minioClient = Minio(
-    config['hostname'],
-    access_key=config['accessKey'],
-    secret_key=config['secretKey'],
-    secure=False
-)
+# minioClient = Minio(
+#     config['hostname'],
+#     access_key=config['accessKey'],
+#     secret_key=config['secretKey'],
+#     secure=False
+# )
 
-try:
-    s3_folder = list(minioClient.list_objects(config['bucket']))[-1].object_name
-    files = minioClient.list_objects(config['bucket'], prefix=s3_folder, recursive=True)
-    for file in files:
-        minioClient.fget_object(config['bucket'], file.object_name, './%s/%s' % (config['src_dir'], file.object_name))
-        log.info('saving %s' % file.object_name)
+# try:
+#     s3_folder = list(minioClient.list_objects(config['bucket']))[-1].object_name
+#     files = minioClient.list_objects(config['bucket'], prefix=s3_folder, recursive=True)
+#     for file in files:
+#         minioClient.fget_object(config['bucket'], file.object_name, './%s/%s' % (config['src_dir'], file.object_name))
+#         log.info('saving %s' % file.object_name)
 
-except ResponseError as err:
-    log.error(err)
-    exit(1)
+# except ResponseError as err:
+#     log.error(err)
+#     exit(1)
 
-# s3_folder = '20191120_10.38.13_catalogueWww/'
+s3_folder = '20191206_09.42.42_catalogueWww/'
 
 config['src_dir'] = path.join(config['src_dir'], s3_folder)
 
-log.info('agegroup.csv                -> lifelines_age_group.tsv')
+
+log.info('{:<30} -> {}'.format('agegroup.csv', 'lifelines_age_group.tsv'))
 age_group = pd.read_csv(path.join(config['src_dir'], 'agegroup.csv'), engine='python')
 age_group.rename(columns={'AGEGROUP_ID': 'id', 'LABEL': 'name'}, inplace=True)
 age_group.to_csv(path.join(config['target_dir'], 'lifelines_age_group.tsv'), sep='\t', index=False)
 
 
-log.info('gender.csv             -> lifelines_gender_group.tsv')
+log.info('{:<30} -> {}'.format('gender.csv', 'lifelines_gender_group.tsv'))
 gender_group = pd.read_csv(path.join(config['src_dir'], 'gender.csv'), engine='python')
 gender_group.rename(
     columns={'GENDER_ID': 'id', 'LABEL': 'name'},
@@ -53,24 +54,44 @@ gender_group['name'] = gender_group['name'].str.lower()
 gender_group.to_csv(path.join(config['target_dir'], 'lifelines_gender_group.tsv'), sep='\t', index=False)
 
 
-log.info('assessment.csv               -> lifelines_assessment.tsv')
+log.info('{:<30} -> {}'.format('assessment.csv', 'lifelines_assessment.tsv'))
 assessment = pd.read_csv(path.join(config['src_dir'], 'assessment.csv'), engine='python')
 assessment.rename(columns={'ASSESSMENT_ID': 'id', 'NAME': 'name'}, inplace=True)
 assessment.to_csv(path.join(config['target_dir'], 'lifelines_assessment.tsv'), sep='\t', index=False)
 
 
-log.info('section.txt                  -> lifelines_section.tsv...')
-df = pd.read_csv(path.join(config['src_dir'], 'section.txt'), sep='\t', engine='python')
-df.rename(columns={'section_id': 'id'}, inplace=True)
-df.to_csv(path.join(config['target_dir'], 'lifelines_section.tsv'), columns=['id', 'name'], sep='\t', index=False)
+log.info('{:<30} -> {}'.format('variable.csv', 'lifelines_section.tsv'))
+variable = pd.read_csv(path.join(config['src_dir'], 'variable.csv'), engine='python')
+sections = pd.concat(objs=[
+    variable[['SECTION_NAME']],
+    variable[['ALT_SECTION_NAME']].rename(columns={'ALT_SECTION_NAME': 'SECTION_NAME'})
+]).dropna().drop_duplicates().sort_values(by=['SECTION_NAME'])\
+    .rename(columns={'SECTION_NAME': 'name'}).reset_index()
 
-log.info('subsection.txt               -> lifelines_sub_section.tsv...')
-df = pd.read_csv(path.join(config['src_dir'], 'subsection.txt'), sep='\t', engine='python')
-df.rename(columns={'subsection_id': 'id'}, inplace=True)
-df.to_csv(path.join(config['target_dir'], 'lifelines_sub_section.tsv'), columns=['id', 'name'], sep='\t', index=False)
+sections.to_csv(
+    path.join(config['target_dir'], 'lifelines_section.tsv'),
+    columns=['name'], sep='\t', index_label='id'
+)
+sections = pd.read_csv(path.join(config['target_dir'], 'lifelines_section.tsv'), sep='\t', engine='python')
 
 
-log.info('variant.csv                  -> lifelines_variant.tsv...')
+log.info('{:<30} -> {}'.format('variable.csv', 'lifelines_subsection.tsv'))
+variable = pd.read_csv(path.join(config['src_dir'], 'variable.csv'), engine='python')
+subsections = pd.concat(objs=[
+    variable[['SUBSECTION_NAME']],
+    variable[['ALT_SUBSECTION_NAME']].rename(columns={'ALT_SUBSECTION_NAME': 'SUBSECTION_NAME'})
+]).dropna().drop_duplicates().sort_values(by=['SUBSECTION_NAME'])\
+    .rename(columns={'SUBSECTION_NAME': 'name'}).reset_index()
+
+subsections.to_csv(
+    path.join(config['target_dir'], 'lifelines_subsection.tsv'),
+    columns=['name'], sep='\t', index_label='id'
+)
+
+subsections = pd.read_csv(path.join(config['target_dir'], 'lifelines_subsection.tsv'), sep='\t', engine='python')
+
+
+log.info('{:<30} -> {}'.format('variant.csv', 'lifelines_variant.tsv'))
 df = pd.read_csv(path.join(config['src_dir'], 'variant.csv'), engine='python')
 df.rename(columns={'VARIANT_ID': 'id', 'NAME': 'name', 'ASSESSMENT_ID': 'assessment_id'}, inplace=True)
 df.to_csv(
@@ -78,7 +99,8 @@ df.to_csv(
     columns=['id', 'name', 'assessment_id'], sep='\t', index=False, float_format='%.f'
 )
 
-log.info('variable.csv + whatwhen.csv -> lifelines_variable.tsv...')
+
+log.info('{:<30} -> {}'.format('variable.csv + whatwhen.csv', 'lifelines_variable.tsv'))
 variable = pd.read_csv(path.join(config['src_dir'], 'variable.csv'), engine='python')
 variable.rename(
     columns={
@@ -99,6 +121,7 @@ grouped = what_when.groupby('id').agg(
     )
 ).reset_index()
 
+
 variable = pd.merge(variable, grouped, on='id', how='left')
 variable.to_csv(
     path.join(config['target_dir'], 'lifelines_variable.tsv'),
@@ -106,34 +129,58 @@ variable.to_csv(
     sep='\t', index=False, float_format='%.f'
 )
 
-log.info('variable.csv                 -> lifelines_tree.tsv')
+
+log.info('{:<30} -> {}'.format('variable.csv', 'lifelines_tree.tsv'))
 variable = pd.read_csv(path.join(config['src_dir'], 'variable.csv'), engine='python')
-subsections = variable[['section_id', 'subsection_id']]
-alt_subsections = variable[['alt_section_id', 'alt_subsection_id']].dropna().astype(
-    {'alt_section_id': 'int32', 'alt_subsection_id': 'int32'}).rename(columns={'alt_section_id': 'section_id',
-                                                                               'alt_subsection_id': 'subsection_id'})
-tree = pd.concat(objs=[subsections, alt_subsections], sort=False).sort_values(
-    by=['section_id', 'subsection_id']).drop_duplicates().reset_index()
+
+section_tree = pd.merge(variable, sections, left_on='SECTION_NAME', right_on='name', how='left').rename(columns={'id': 'section_id'})
+section_tree = pd.merge(section_tree, subsections, left_on='SUBSECTION_NAME', right_on='name', how='left').rename(columns={'id': 'subsection_id'})
+
+alt_section_tree = pd.merge(
+    variable, sections, left_on='ALT_SECTION_NAME', right_on='name', how='left').rename(columns={'id': 'section_id'}
+)
+alt_section_tree = pd.merge(
+    alt_section_tree, subsections, left_on='ALT_SUBSECTION_NAME', right_on='name', how='left'
+).rename(columns={'id': 'subsection_id'})
+
+tree = pd.concat(objs=[
+    section_tree[['section_id', 'subsection_id']],
+    alt_section_tree[['section_id', 'subsection_id']],
+    ], sort=False).dropna().astype({
+        'section_id': 'int32',
+        'subsection_id': 'int32',
+    }).sort_values(by=['section_id', 'subsection_id']).drop_duplicates().reset_index()
 tree.to_csv(
     path.join(config['target_dir'], 'lifelines_tree.tsv'),
     columns=['section_id', 'subsection_id'], sep='\t', index_label='id'
 )
 
-log.info('variable.csv                 -> lifelines_subsection_variable.tsv')
+
+log.info('{:<30} -> {}'.format('variable.csv', 'lifelines_subsection_variable.tsv'))
 variable = pd.read_csv(path.join(config['src_dir'], 'variable.csv'), engine='python')
-subvars = variable[['subsection_id', 'variable_id']]
-alt_subvars = variable[['alt_subsection_id', 'variable_id']].dropna().astype(
-    {'alt_subsection_id': 'int32'}).rename(columns={'alt_subsection_id': 'subsection_id'})
-subsection_variable = pd.concat(objs=[subvars, alt_subvars], sort=False).sort_values(
-    by=['subsection_id', 'variable_id']).drop_duplicates().reset_index()
-subsection_variable.to_csv(config['target_dir'] + 'lifelines_subsection_variable.tsv',
+
+subvars = pd.merge(variable, subsections, left_on='SUBSECTION_NAME', right_on='name', how='left')\
+    .rename(columns={'id': 'subsection_id', 'VARIABLE_ID': 'variable_id'})
+
+alt_subvars = pd.merge(
+    variable, subsections, left_on='ALT_SUBSECTION_NAME', right_on='name', how='left')\
+    .rename(columns={'id': 'subsection_id', 'VARIABLE_ID': 'variable_id'})
+
+subsection_variable = pd.concat(objs=[
+    subvars[['subsection_id', 'variable_id']],
+    alt_subvars[['subsection_id', 'variable_id']]
+], sort=False).dropna().astype({
+    'subsection_id': 'int32',
+    'variable_id': 'int32',
+}).sort_values(by=['subsection_id', 'variable_id']).drop_duplicates().reset_index()
+subsection_variable.to_csv(path.join(config['target_dir'], 'lifelines_subsection_variable.tsv'),
                            columns=['subsection_id', 'variable_id'], sep='\t', index_label='id')
 
-log.info('who.csv                      -> lifelines_who.tsv')
+
+log.info('{:<30} -> {}'.format('who.csv', 'lifelines_who.tsv'))
 who = pd.read_csv(path.join(config['src_dir'], 'who.csv'), engine='python')
 who.rename(
     columns={
-        'PARTICIPANT_ID': 'll_nr',
         'GENDER': 'gender_group',
         'AGE_GROUP_AT1A': 'age_group_at_1a',
         'AGE_GROUP_AT2A': 'age_group_at_2a',
@@ -149,21 +196,20 @@ who.rename(
 who.to_csv(
     path.join(config['target_dir'], 'lifelines_who.tsv'),
     columns=[
-        'll_nr', 'gender_group', 'age_group_at_1a', 'age_group_at_2a', 'age_group_at_3a', 'year_of_birth',
+        'gender_group', 'age_group_at_1a', 'age_group_at_2a', 'age_group_at_3a', 'year_of_birth',
         'subcohortgwas_group', 'subcohortugli_group', 'subcohortdeep_group', 'subcohortdag3_group'
     ],
-    float_format='%.f', sep='\t', index=False
+    float_format='%.f', sep='\t',
+    index_label='ll_nr'
 )
-# PARTICIPANT_ID,VARIANT_ID,GENDER_TXT,AGE_INT
-log.info('whowhen.csv                 -> lifelines_who_when.tsv')
+
+
+log.info('{:<30} -> {}'.format('whowhen.csv', 'lifelines_who_when.tsv'))
 who_when = pd.read_csv(path.join(config['src_dir'], 'whowhen.csv'), engine='python')
-who_when.rename(
-    columns={
-        'PARTICIPANT_ID': 'll_nr',
-        'VARIANT_ID': 'variant_id'
-    },
-    inplace=True
-)
+who['ll_nr'] = who.index
+
+who_when = pd.merge(who, who_when, on='PARTICIPANT_ID', how='inner')
+who_when.rename(columns={'VARIANT_ID': 'variant_id'}, inplace=True)
 who_when.to_csv(
     path.join(config['target_dir'], 'lifelines_who_when.tsv'),
     sep='\t', index_label='id', columns=['ll_nr', 'variant_id']
